3d724b0762da523ef693ab528b3afedd6f403bd9
alesiods <alesioadrover@gmail.com>
2023-04-27 19:39:24 -0300

    primer commmit

---
+++ main.js
@@ -0,0 +1,28 @@
+let nombre= prompt("Ingresa nombre");
+let apellido= prompt("Ingresa apellido");
+
+if(nombre!= "" && apellido!=""){
+let mensaje= " Bienvenido/a " + nombre +" "+ apellido;
+alert(mensaje);
+}else{
+alert("Ingrese nombre y apellido")
+}
+
+
+for(let turno= 1;turno <= 2; turno ++){
+let numero= prompt("Ingrese DNI")
+alert("Su turno es el" + ""+ turno);
+}
+alert("No hay mas turnos")
+
+
+let encuentro=("Tiempo de espera para el turno")
+function tiempo(a, b ){
+
+let resultado= a + b;
+console.log("Tiempo"+ resultado );
+}
+
+
+
+tiempo(10 , 20);
